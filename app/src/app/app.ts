a35a858db739431ab00bd4ac06fcf5b90ccfae4d
Lucas <lucasax147@gmail.com>
2025-08-15 21:21:39 -0300

    Estrutura Login

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from "./login/login";
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [Login],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
