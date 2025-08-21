8f41a3bed9d0d0764ea9c8c299b5026c151650b2
Lucas <lucasax147@gmail.com>
2025-08-21 17:17:53 -0300

    Rotas e guard

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { rotaGuard } from './autenticacao/rota-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'home', component: Home, canActivate: [rotaGuard] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
