dfe8f4f62907d946327b2b38b7a4f783c25a2d48
Lucas <lucasax147@gmail.com>
2025-08-26 23:51:28 -0300

    feat: Add login functionality

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
-import { Home } from './pages/home/home';
+
+
 import { rotaGuard } from './autenticacao/rota-guard';
+import { Login } from './pages/login/login.component';
+import { Home } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
-  { path: 'home', component: Home }/*, canActivate: [rotaGuard] },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
-];*/
+  { path: '', component: Home }
 ]
--- app/src/app/pages/login/login.component.spec.ts
+++ app/src/app/pages/login/login.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Home } from './home';
+import { Login } from './login.component';
 
-describe('Home', () => {
-  let component: Home;
-  let fixture: ComponentFixture<Home>;
+describe('Login', () => {
+  let component: Login;
+  let fixture: ComponentFixture<Login>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Home]
+      imports: [Login]
     })
-    .compileComponents();
+      .compileComponents();
 
-    fixture = TestBed.createComponent(Home);
+    fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
